e9ce08178c293623235987bcb354f7f1cc658057
turkergoksu <turkergoksu@gmail.com>
2021-01-27 19:27:51 +0300

    Added Orbit class.

--- app/src/main/java/me/turkergoksu/socialorbitlayout/MainActivity.kt
+++ app/src/main/java/me/turkergoksu/socialorbitlayout/MainActivity.kt
@@ -1,5 +1,7 @@
 package me.turkergoksu.socialorbitlayout
 
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val orbit = Orbit(mutableListOf())
+        for (i in 0..7) {
+            val floatingObject = FloatingObject(
+                Color.WHITE,
+                BitmapFactory.decodeResource(resources, R.drawable.dummy1)
+            )
+            orbit.floatingObjects?.add(floatingObject)
+        }
     }
 }
